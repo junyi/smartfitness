c1eea7bb74a8c5c563128785a2dfcdfaffe95ef2
Hee Jun Yi <junyi.hjy@gmail.com>
2015-07-01 10:48:11 +0800

    Added base files for LaunchActivity

--- app/src/main/java/astar/smartfitness/LaunchActivity.java
+++ app/src/main/java/astar/smartfitness/LaunchActivity.java
@@ -1,22 +1,24 @@
 package astar.smartfitness;
 
 import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class LaunchActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_launch, menu);
         return true;
     }
 
